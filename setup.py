eb943b37558007c69739d8afdb72fc150bd9c62c
Rhydian Windsor <windsorrhydian@gmail.com>
2021-08-09 13:09:10 +0100

    Getting ready for PyPi upload

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(name= 'dicomcat',
-      version= '0.1',
+      version= '0.2',
       description='A simple python-based tool based on imgcat for displaying DICOM files in iTerm2.',
+      long_description_content_type='text/markdown',
+      long_description='README.md',
       url='https://github.com/rwindsor1/DICOMcat',
       author ='Rhydian Windsor',
       author_email= 'windsorrhydian@gmail.com',
